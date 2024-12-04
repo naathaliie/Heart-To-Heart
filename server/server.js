0690a2938bda1b2084837531c96ee0eec4bedd55
naathaliie <nathalielarsson@live.se>
2024-12-04 15:16:54 +0100

    updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import Express from "express";
 import { testRouter } from "./routes/testRuoter.js";
 import { connectToDataBase } from "./dataBase/connectToDataBase.js";
+import { levelsRouter } from "./routes/levelsRouter.js";
+import { categoriesRouter } from "./routes/categoriesRouter.js";
+import { questionsRouter } from "./routes/questionsRouter.js";
 
 const app = Express();
 const port = 3003;
@@ -25,8 +28,11 @@ app.use("*", async (req, res, next) => {
     next();
 });
 
-//Få tillgång till alla routes
+//Få tillgång till alla endpoints
 app.use("/test", testRouter());
+app.use("/levels", levelsRouter());
+app.use("/categories", categoriesRouter());
+app.use("/questions", questionsRouter());
 
 
 
@@ -34,31 +40,3 @@ app.listen(port,() => {
     console.log(`port is running on ${port}`);
 });
 
-
-/* //Hämta ALLA
-app.get("/", (req, res) => {
-    res.send(testArray);
-})
-
-//Posta till ALLA
-app.post("/", (req, res) => {
- //Använd safeParse för att validera indatan på ett mer elegant sätt än med bara parse
- const result = testSchema.safeParse(req.body);
- 
- if (result.success) {
-    //Om valideringen lyckas, lägg till den nya resursen i testArrayen
-    testArray.push(result.data);
-
-    //Ge tillbaka resursen som ett svar (för att bekräfta att det gick) med status 201
-    res.status(201).json({
-        message: "Datan var giltig och har nu sparats!",
-        data: result.data,
-    });
- } else {
-    //Om valideringen misslyckades, skicka en 400-status med felmeddelande
-    res.status(400).json({
-        message: 'Valideringsfel',
-        errors: result.error.errors // Detta innehåller specifika felmeddelanden från zod
-    });
- }
-}) */

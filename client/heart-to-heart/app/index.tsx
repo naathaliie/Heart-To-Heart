5da0b7d07d286fdcf13dd213e4383585e08333da
naathaliie <nathalielarsson@live.se>
2024-12-05 14:14:24 +0100

    welcomePage(index)

--- client/heart-to-heart/app/index.tsx
+++ client/heart-to-heart/app/index.tsx
@@ -1,22 +1,27 @@
-import { Link, useRouter } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
-
+import { useRouter } from "expo-router";
+import { Pressable, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
 export default function Index() {
   //Komma åt routern
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Text>index</Text>
-      <Link href={"/changeLevel"}>
-        <Pressable
-          onPress={() => {
-            router.push("/(tabs)/homeScreen");
-          }}
-        >
-          <Text>Starta appen</Text>
-        </Pressable>
-      </Link>
+      <StatusBar barStyle={"dark-content"} backgroundColor={"#292057"} />
+      <Image
+        source={require("../assets/images/welcomeImg.jpeg")}
+        style={styles.imgStyle}
+        contentFit="cover"
+        allowDownscaling={true}
+      />
+      <Pressable
+        style={styles.btn}
+        onPress={() => {
+          router.push("/(tabs)/homeScreen");
+        }}
+      >
+        <Text style={styles.btnText}>Gå till appen</Text>
+      </Pressable>
     </View>
   );
 }
@@ -26,5 +31,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#25292c",
+  },
+  imgStyle: {
+    flex: 1, // Täcker hela föräldraview
+    width: "100%",
+    height: "100%",
+    position: "absolute", // Gör att bilden fyller hela skärme
+    backgroundColor: "#0553",
+  },
+  linkStyle: {
+    color: "white",
+    textDecorationLine: "underline",
+    fontSize: 20,
+  },
+  btn: {
+    backgroundColor: "#08AEAD",
+    borderWidth: 1,
+    borderColor: "#ffffff",
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 500,
+  },
+  btnText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });

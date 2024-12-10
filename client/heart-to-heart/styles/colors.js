2926a97fa7c0004456d72e00dfcaea6765702b53
naathaliie <nathalielarsson@live.se>
2024-12-10 10:46:46 +0100

    added styles-colors

---
+++ client/heart-to-heart/styles/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    dustyCherry: "#D3939D",
+    dustyCherry_Dark: "#CC828E",
+    dustyCherry_Light: "#EAD0D3",
+    deliciousGreen: "#94C480",
+    velvet: "#E87788",
+    silver: "#F8F8F8",
+    myBlack: "#2B2B2B"
+
+};
+
+export default colors;

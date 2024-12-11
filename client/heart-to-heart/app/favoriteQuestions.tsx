e44fd5672433bf9e33f340cb98d9135c3fd6de9d
naathaliie <nathalielarsson@live.se>
2024-12-11 21:43:07 +0100

    .

--- client/heart-to-heart/app/favoriteQuestions.tsx
+++ client/heart-to-heart/app/favoriteQuestions.tsx
@@ -1,13 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 import colors from "../styles/colors.js";
 import fonts from "../styles/fonts.js";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/redux/store.js";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store.js";
+import { useEffect } from "react";
 
 export default function FavoriteQuestions() {
-  const dispatch = useDispatch<AppDispatch>();
   //komma åt currentUser och all denns info
   const currentUser = useSelector((state: RootState) => state.currentUser);
+
+  //För att uppdatera om en ny fråga blir gillamarkerad/borttagen
+  useEffect(() => {
+    if (currentUser.currentUser) {
+      console.log(
+        "Uppdaterade gillade frågor:",
+        currentUser.currentUser.likedQuestions
+      );
+    }
+  }, [currentUser.currentUser?.likedQuestions]);
 
   return (
     <View style={styles.container}>

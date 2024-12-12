0449fd1daa6cb4848de4b65c2c12bbc6439f88ce
naathaliie <nathalielarsson@live.se>
2024-12-12 09:38:01 +0100

    functionality is done

--- client/heart-to-heart/app/favoriteQuestions.tsx
+++ client/heart-to-heart/app/favoriteQuestions.tsx
@@ -4,16 +4,25 @@ import fonts from "../styles/fonts.js";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/redux/store.js";
 import { useEffect, useState } from "react";
+import { fetchAllFavoritQuestions } from "@/API/api.ts";
 
 export default function FavoriteQuestions() {
   const dispatch = useDispatch<AppDispatch>();
   const currentUser = useSelector((state: RootState) => state.currentUser);
 
-/
+  //Hämtar favoritQuestions från redux userstate?
+  const { users } = useSelector((state: RootState) => state.users);
 
   useEffect(() => {
-   //ladda in alla favoritfrågor från backend
-  }, []); //Lyssna på förändringar om allafavoritfrågor ändras
+    //ladda in alla favoritfrågor från backend
+    if (currentUser.currentUser) {
+      dispatch(fetchAllFavoritQuestions(currentUser.currentUser?._id));
+    }
+  }, [dispatch]); //Lyssna på förändringar om allafavoritfrågor ändras
+
+  const thisUsersFavoriteQuestions =
+    users.find((user) => user._id === currentUser.currentUser?._id)
+      ?.likedQuestions || [];
 
   return (
     <View style={styles.container}>
@@ -21,8 +30,8 @@ export default function FavoriteQuestions() {
         <Text style={styles.text}>Dina favvofrågor</Text>
       </View>
       <View>
-        {favoritQuestions ? (
-          favoritQuestions.map((q) => {
+        {thisUsersFavoriteQuestions.length > 0 ? (
+          thisUsersFavoriteQuestions.map((q) => {
             return (
               <View key={q._id}>
                 <Text>{q.questionText}</Text>

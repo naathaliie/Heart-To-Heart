7f260f4e71d2cd83eb994351d601737b4ce6b2f7
naathaliie <nathalielarsson@live.se>
2024-12-12 17:02:12 +0100

    scrollview and styling

--- client/heart-to-heart/app/favoriteQuestions.tsx
+++ client/heart-to-heart/app/favoriteQuestions.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import colors from "../styles/colors.js";
 import fonts from "../styles/fonts.js";
 import { useDispatch, useSelector } from "react-redux";
@@ -10,11 +10,9 @@ export default function FavoriteQuestions() {
   const dispatch = useDispatch<AppDispatch>();
   const currentUser = useSelector((state: RootState) => state.currentUser);
 
-  //Hämtar favoritQuestions från redux userstate?
   const { users } = useSelector((state: RootState) => state.users);
 
   useEffect(() => {
-    //ladda in alla favoritfrågor från backend
     if (currentUser.currentUser) {
       dispatch(fetchAllFavoritQuestions(currentUser.currentUser?._id));
     }
@@ -29,35 +27,67 @@ export default function FavoriteQuestions() {
       <View style={styles.textBox}>
         <Text style={styles.text}>Dina favvofrågor</Text>
       </View>
-      <View>
-        {thisUsersFavoriteQuestions.length > 0 ? (
-          thisUsersFavoriteQuestions.map((q) => {
-            return (
-              <View key={q._id}>
-                <Text>{q.questionText}</Text>
-              </View>
-            );
-          })
-        ) : (
-          <Text> Du har inte gillamarkerat några frågor ännu</Text>
-        )}
-      </View>
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 80, // Extra utrymme längst ner för att visa sista frågan
+          paddingTop: 20,
+        }}
+      >
+        <View style={styles.favQuestionBox}>
+          {thisUsersFavoriteQuestions.length > 0 ? (
+            thisUsersFavoriteQuestions.map((q) => {
+              return (
+                <View key={q._id} style={styles.favQuestion}>
+                  <Text style={styles.questionText}>{q.questionText}</Text>
+                  <Text style={styles.categoryText}>
+                    {" "}
+                    Kategori: {q.categoryType}
+                  </Text>
+                </View>
+              );
+            })
+          ) : (
+            <Text> Du har inte gillamarkerat några frågor ännu</Text>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.myBackground,
-    flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    paddingBottom: 80,
+    paddingTop: 20,
+    backgroundColor: colors.myBackground,
   },
   textBox: {
+    justifyContent: "center",
     marginBottom: 20,
   },
   text: {
     fontSize: fonts.fontSizes.medium,
     textAlign: "center",
+  },
+  favQuestionBox: {
+    padding: 10,
+  },
+  favQuestion: {
+    backgroundColor: colors.dustyCherry_Dark,
+    margin: 7,
+    padding: 15,
+    width: "85%",
+    borderRadius: 20,
+    gap: 5,
+  },
+  questionText: {
+    fontSize: fonts.fontSizes.medium,
+    color: colors.silver,
+  },
+  categoryText: {
+    color: colors.silver,
+    fontSize: fonts.fontSizes.small,
+    fontStyle: "italic",
   },
 });

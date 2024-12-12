f862d1716321fc6125020760a6f4dec926bd151c
naathaliie <nathalielarsson@live.se>
2024-12-12 22:45:13 +0100

    added modal

--- client/heart-to-heart/app/loginScreen.tsx
+++ client/heart-to-heart/app/loginScreen.tsx
@@ -5,6 +5,7 @@ import {
   Pressable,
   TextInput,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import { useRouter } from "expo-router";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,10 +15,17 @@ import { NewUser, User } from "@/types";
 import { updateCurrentUser } from "@/redux/currentUserSlice";
 import { addNewUser, fetchUsers } from "@/API/api";
 import PrimaryBtn from "@/components/primaryBtn";
-import colors from "@/styles/colors";
 import fonts from "@/styles/fonts.js";
+import { Modal, Portal, Button, PaperProvider } from "react-native-paper";
 
 export default function loginScreen() {
+  /***MODAL-STUFF***/
+  const [visible, setVisible] = useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  /***MODAL-STUFF***/
+
   const router = useRouter();
   const dispatch = useDispatch<AppDispatch>();
 
@@ -26,10 +34,8 @@ export default function loginScreen() {
     (state: RootState) => state.users
   );
 
-  //hantera input från inputfältet:
   const [inputWord, setInputword] = useState("");
 
-  //Se vald profil man vill logga in med
   const [chosedProfile, setChosedProfile] = useState<User | undefined>(
     undefined
   );
@@ -39,16 +45,13 @@ export default function loginScreen() {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  //hantera gå till appen-knappen
   function handleGoToApp() {
-    // Dispatcha action för att uppdatera currentUser i Redux
     if (chosedProfile) {
-      dispatch(updateCurrentUser(chosedProfile)); // Uppdaterar Redux store med den valda användaren
+      dispatch(updateCurrentUser(chosedProfile));
     }
     router.replace("/levelsScreen");
   }
 
-  // Hantera lägg till användare
   const handleAddUser = async () => {
     console.log("du klickade på handleAddUser för att lägga till :", inputWord);
 
@@ -64,7 +67,7 @@ export default function loginScreen() {
     // När användaren är tillagd, hämta den uppdaterade listan
     await dispatch(fetchUsers());
 
-    // Logga de uppdaterade användarna
+    hideModal();
   };
 
   if (loading) {
@@ -76,70 +79,80 @@ export default function loginScreen() {
   }
 
   return (
-    <ScrollView
-      contentContainerStyle={{
-        paddingBottom: 30,
-      }}
-    >
-      <View style={styles.container}>
-        <View style={styles.existingProfilesContainer}>
-          <Text style={{ fontSize: fonts.fontSizes.medium }}>
-            Välj en profil
-          </Text>
-
-          <View style={styles.existingProfilesBox}>
-            {users.map((user) => {
-              return (
-                <PrimaryBtn
-                  key={user._id + user.username}
-                  title={user.username}
-                  onPress={() => setChosedProfile(user)}
-                  color="dustyCherryDark"
-                  size="small"
-                />
-              );
-            })}
-          </View>
-          <Text style={{ fontSize: fonts.fontSizes.small }}>
-            Du kommer nu att logga in som: {chosedProfile?.username}
-          </Text>
-          <PrimaryBtn
-            title="Gå till appen"
-            onPress={() => handleGoToApp()}
-            color="deliciousGreen"
-            size="big"
-          />
-        </View>
-
-        {/* KANSKE EN MODAL AV NEDAN? */}
-
-        <Text style={{ margin: 20, fontWeight: "bold" }}>Eller</Text>
-
-        <View style={styles.newProfileContainer}>
-          <View>
-            <Text>Skapa en ny användare</Text>
-          </View>
-          <View>
-            <TextInput
-              style={styles.inputField}
-              onChangeText={(input) => setInputword(input)}
-              value={inputWord}
-              placeholder="Användarnamn: "
-              keyboardType="default"
-            />
-            <Text>DU SKREV: {inputWord}</Text>
-            <Pressable
-              onPress={() => {
-                handleAddUser();
+    <PaperProvider>
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 30,
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.existingProfilesContainer}>
+            <Text
+              style={{
+                fontSize: fonts.fontSizes.medium,
+                textAlign: "center",
+                marginBottom: 20,
               }}
-              style={styles.btn}
             >
-              <Text>Skapa ny användare</Text>
-            </Pressable>
+              Välj en användare och logga in
+            </Text>
+
+            <View style={styles.existingProfilesBox}>
+              {users.map((user) => {
+                return (
+                  <PrimaryBtn
+                    key={user._id + user.username}
+                    title={user.username}
+                    onPress={() => setChosedProfile(user)}
+                    color="dustyCherryDark"
+                    size="small"
+                  />
+                );
+              })}
+            </View>
+            <PrimaryBtn
+              title={
+                chosedProfile?.username
+                  ? `Logga in som ${chosedProfile?.username}`
+                  : "Välj profil"
+              }
+              onPress={() => handleGoToApp()}
+              color="deliciousGreen"
+              size="big"
+            />
           </View>
+
+          {/* KANSKE EN MODAL AV NEDAN? */}
+
+          <Portal>
+            <Modal
+              visible={visible}
+              onDismiss={hideModal}
+              contentContainerStyle={styles.modalStyle}
+            >
+              <View style={{ alignItems: "center" }}>
+                <TextInput
+                  style={styles.inputField}
+                  onChangeText={(input) => setInputword(input)}
+                  value={inputWord}
+                  placeholder="Nytt användarnamn "
+                  keyboardType="default"
+                />
+                <PrimaryBtn
+                  title="Skapa ny användare"
+                  onPress={() => handleAddUser()}
+                  color="deliciousGreen"
+                  size="big"
+                />
+              </View>
+            </Modal>
+          </Portal>
+          <Button style={{ marginTop: 30 }} onPress={showModal}>
+            Skapa en ny användare
+          </Button>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </PaperProvider>
   );
 }
 
@@ -189,5 +202,16 @@ const styles = StyleSheet.create({
   inputField: {
     borderWidth: 2,
     width: "60%",
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+  modalStyle: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10, // Gör hörnen rundade
+    width: Dimensions.get("window").width * 0.8, // Gör den 80% av skärmens bredd
+    height: Dimensions.get("window").height * 0.7,
+    maxWidth: 600, // Maxbredd för större skärmar
+    alignSelf: "center", // Centrerar modalen horisontellt
   },
 });
